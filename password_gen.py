362f48d90ab7d50ac7ec4e67e2caa4390ef5c911
Ebrahim_ak <y560mia3@gmail.com>
2024-05-23 19:49:46 +0330

    nothing seriuos added

--- password_gen.py
+++ password_gen.py
@@ -22,14 +22,15 @@ while True:
 
     def space_pass():
         return ' '
-    
+
     def check_setting(sett):
-        set_of_main = {1: upper_pass(), 2: lower_pass(), 3: digit_pass(), 4: symbol_pass(), 5: space_pass()}
-        
+        set_of_main = {1: upper_pass(), 2: lower_pass(
+        ), 3: digit_pass(), 4: symbol_pass(), 5: space_pass()}
+
         def decide(item):
-            a , b = item
+            a, b = item
             return sett[a] == 'true'
-        set_of_main = dict(filter(decide , set_of_main.items()))
+        set_of_main = dict(filter(decide, set_of_main.items()))
         return set_of_main
 
     def create_random_place(sett):
@@ -58,7 +59,7 @@ while True:
             print('program finished')
             flag1 = True
         elif not (item.isdigit()) and item not in ['true', 'false', 'done']:
-            flag= True
+            flag = True
             break
     if flag == True:
         print('invalid input!!! , try again')
@@ -75,20 +76,17 @@ while True:
 
     passwd = run_me()
 # passwd = [('Xy6vT82Hkr1qW24',), ('A3egL3ZYXiR76d0',), ('6RjSWeMhaDU0j7a',), ('bS0g71bI3UvQerk',), ('1Cn61B04NVYqrVd',), ('2KlBl9yuD95Li2F',)]
-mydb = mysql.connector.connect(user='ebrahimak',password='admin@1990',host='localhost')
-cursor=mydb.cursor()
+mydb = mysql.connector.connect(
+    user='ebrahimak', password='admin@1990', host='localhost')
+cursor = mydb.cursor()
 cursor.execute('create database if not exists passwords')
 cursor.execute('use passwords')
 cursor.execute('create table if not exists pass('
                'id int auto_increment primary key,'
                'password varchar(100))')
-# for item in passwd:
-#     cursor.execute('insert into pass (password) values (%s)',list(item))
-    
-cursor.executemany('insert into pass (password) values (%s)',passwd)
+
+cursor.executemany('insert into pass (password) values (%s)', passwd)
 mydb.commit()
 cursor.execute('select * from pass')
 passwords = cursor.fetchall()
 print(passwords)
-
-
